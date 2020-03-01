54def60511281b91b0ac9c1c93028f5bb9fc718f
Abhay Vincent <abhaykvincent@gmail.com>
2020-03-01 02:06:14 -0500

    Product display buttons

--- js/app.js
+++ js/app.js
@@ -4,28 +4,161 @@ let products=[
     {
         name : "Soap",
         price: 10,
+        img: "product1"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product2"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product3"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product4"
+    },
 
-    }
+    
+    
+    {
+        name : "Soap",
+        price: 10,
+        img: "product4"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product3"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product1"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product2"
+    },{
+        name : "Soap",
+        price: 10,
+        img: "product1"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product2"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product3"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product4"
+    },
+
+    
+    
+    {
+        name : "Soap",
+        price: 10,
+        img: "product4"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product3"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product1"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product2"
+    },{
+        name : "Soap",
+        price: 10,
+        img: "product1"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product2"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product3"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product4"
+    },
+
+    
+    
+    {
+        name : "Soap",
+        price: 10,
+        img: "product4"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product3"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product1"
+    },
+    {
+        name : "Soap",
+        price: 10,
+        img: "product2"
+    },
 ]
 //function definitions
 function displayProducts(){
-    content=`<div class="small-3 productContainer">
-    <div class="product">
-        <div class="productImagenExtra"></div>
-        <div class="addToCart_Panel">
-            <div class="counter">
-                <div class="minus"><i class="fas fa-minus-square"></i></div>
-                <p  class="count">1</p>
-                <div class="add"><i class="fas fa-plus-square"></i></div>
-            </div>
-            <div class="addToCart_button"><p>Add To Cart</p></div>
-        </div>
-    </div>
-</div>`;
-for(i=0;i<30;i++){
+    content="";
+    products.forEach(product => {
+        
+        content+=`<div class="small-3 productContainer">
+                    <div class="product">
+                    <p class="productPrice">$ ${product.price}</p>
+                        <div class="productImagenExtra" style="background-image: url('./img/${product.img}.jpg');">
+                            <p class="productName">${product.name}</p>
+                        </div>
+                        <div class="addToCart_Panel">
+                            <div class="counter">
+                                <div class="minus"><i class="fas fa-minus-square"></i></div>
+                                <p  class="count">1</p>
+                                <div class="add"><i class="fas fa-plus-square"></i></div>
+                            </div>
+                            <div class="addToCart_button"><p>Add To Cart</p></div>
+                        </div>
+                    </div>
+                </div>`;
+    });
     $(".products").append(content);
+    //Functionality
+    $(".add").click(function (e) { 
+        e.preventDefault();
+        
+    });
 }
-}
+        
 $(document).ready(function () {
 displayProducts();
 });
